f10e3b54fef91e0f5fb4fcb9e3506fa1547956e6
Mariam-Levy <mariamlevycode@gmail.com>
2024-01-30 14:46:28 -0300

    added hover and dropdown effect in the navbar

---
+++ dropdown.js
@@ -0,0 +1,29 @@
+
+/* const dropdownContent = document.getElementById('dropdown-content');
+
+function dropdown() {
+    dropdownContent.classList.toggle('dropdown-content--show');
+} */
+
+document.getElementById('dropdown-li').addEventListener('click', function() {
+    document.getElementById('dropdown-content').classList.toggle('dropdown-content--show')
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
