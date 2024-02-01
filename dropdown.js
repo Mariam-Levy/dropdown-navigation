2f6be23123409c4ce9e33e1e55c3b5d543371f83
Mariam-Levy <mariamlevycode@gmail.com>
2024-01-31 21:08:37 -0300

    added dropdown styles and functionality

--- dropdown.js
+++ dropdown.js
@@ -1,18 +1,27 @@
 
-/* const dropdownContent = document.getElementById('dropdown-content');
-
-function dropdown() {
-    dropdownContent.classList.toggle('dropdown-content--show');
-} */
-
-document.getElementById('dropdown-li').addEventListener('click', function() {
-    document.getElementById('dropdown-content').classList.toggle('dropdown-content--show')
-})
 
 
+const dropdownItems = document.querySelectorAll('.dropdown-item');
 
+dropdownItems.forEach(function(dropdownItem) {
+    const dropdownContent = dropdownItem.nextElementSibling;
+    const arrowIcon = dropdownItem.querySelector('.arrow-icon');
 
+    dropdownItem.addEventListener('click', function() {
+        dropdownContent.classList.toggle('dropdown-content--show');
+        arrowIcon.classList.toggle('arrow-rotate');
+    });
 
+    // Cerrar el menú cuando el usuario hace clic fuera de él
+    window.addEventListener('click', function(e) {
+        if (!e.target.matches('.dropdown-item')) {
+            if (dropdownContent.classList.contains('dropdown-content--show')) {
+                dropdownContent.classList.remove('dropdown-content--show');
+                arrowIcon.classList.remove('arrow-rotate');
+            }
+        }
+    });
+});
 
 
 
