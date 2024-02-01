8aa41395ee8616daf31ff02abf6b2c2393d08d56
Mariam-Levy <mariamlevycode@gmail.com>
2024-02-01 01:14:05 -0300

    hero section finished

--- dropdown.js
+++ dropdown.js
@@ -1,23 +1,38 @@
 
-
-
+// Selecciona todos los elementos con la clase 'dropdown-item'
 const dropdownItems = document.querySelectorAll('.dropdown-item');
 
+// Variable para rastrear el último menú desplegable abierto
+let lastOpenedDropdown = null;
+
+// Itera sobre cada elemento con la clase 'dropdown-item'
 dropdownItems.forEach(function(dropdownItem) {
+    // Obtiene el elemento del menú desplegable siguiente
     const dropdownContent = dropdownItem.nextElementSibling;
     const arrowIcon = dropdownItem.querySelector('.arrow-icon');
 
+    // Agrega un evento de clic al elemento del menú desplegable
     dropdownItem.addEventListener('click', function() {
+        // Cierra el último menú desplegable abierto, si existe y si no es el mismo
+        if(lastOpenedDropdown && lastOpenedDropdown !== dropdownContent) {
+            lastOpenedDropdown.classList.remove('dropdown-content--show');
+            lastOpenedDropdown.previousElementSibling.querySelector('.arrow-icon').classList.remove('arrow-rotate');
+        }
+
         dropdownContent.classList.toggle('dropdown-content--show');
         arrowIcon.classList.toggle('arrow-rotate');
+
+        // Actualiza la variable para rastrear el último menú desplegable abierto
+        lastOpenedDropdown = dropdownContent;
     });
 
     // Cerrar el menú cuando el usuario hace clic fuera de él
     window.addEventListener('click', function(e) {
         if (!e.target.matches('.dropdown-item')) {
-            if (dropdownContent.classList.contains('dropdown-content--show')) {
-                dropdownContent.classList.remove('dropdown-content--show');
-                arrowIcon.classList.remove('arrow-rotate');
+            if(lastOpenedDropdown) {
+                lastOpenedDropdown.classList.remove('dropdown-content--show');
+                lastOpenedDropdown.previousElementSibling.querySelector('.arrow-icon').classList.remove('arrow-rotate');
+                lastOpenedDropdown = null;
             }
         }
     });
